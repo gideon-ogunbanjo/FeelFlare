7377be393d290ae42710471c122fa16fe1726228
Gideon-Ogunbanjo <dejiogunbanjo9@gmail.com>
2023-08-10 11:38:13 +0100

    created function to perform Sentiment Analysis

--- App/app.py
+++ App/app.py
@@ -1,3 +1,18 @@
 # importing Libraries
 import streamlit as st
 from textblob import TextBlob
+
+# Function to perform Sentiment Analysis
+
+def analyze_sentiment(text):
+    analysis = TextBlob(text)
+    polarity = analysis.sentiment.polarity
+    
+    if polarity > 0:
+        sentiment = "positive"
+    elif polarity < 0:
+        sentiment = "negative"
+    else:
+        sentiment = "neutral"
+    
+    return sentiment, polarity
